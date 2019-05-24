d75af54ef48eff29c06704c2999e1568d759007e
Joshua Kevin Rachmadi <jokevin.rachmadi@gmail.com>
2019-05-24 19:52:49 +0700

    set unity

---
+++ Assets/autoMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoMove : MonoBehaviour {
+
+    private GameObject thisObject;
+    // Use this for initialization
+    public float moveX;
+    public float moveY;
+    public float moveZ;
+    private float brake;
+    private float accelerate;
+    void Start () {
+        thisObject = this.gameObject;
+        accelerate = -0.01f;
+        brake = -0.02f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (moveZ < 0) {
+                moveZ -= brake;
+            }
+            
+        }
+        else {
+            moveZ += accelerate;
+        }
+        Vector3 moveVector = new Vector3(moveX,moveY,moveZ);
+        thisObject.transform.Translate(moveVector);
+	}
+
+    public Vector3 getPlayerVector() {
+        return new Vector3(3 * moveX, 3 * moveY, 3 * moveZ);
+    }
+}
---
+++ Assets/cameraMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+    private Vector3 offset;
+    private GameObject cameraObject;
+    public GameObject playerObject;
+    private autoMove playerMove;
+    //private float zVector;
+	// Use this for initialization
+	void Start () {
+        cameraObject = this.gameObject;
+        offset = cameraObject.transform.position - playerObject.transform.position;
+        playerMove = playerObject.GetComponent<autoMove>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+
+        //    yVector = 1f;
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    yVector = -1f;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    xVector = +1f;
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    xVector = -1f;
+        //}
+
+        cameraObject.transform.position = playerObject.transform.position + offset - playerMove.getPlayerVector();
+    }
+
+   
+}
